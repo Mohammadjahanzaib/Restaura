73a37eca15ab61864beef2b57890d87995406cfc
Mohammadjahanzaib <157308796+Mohammadjahanzaib@users.noreply.github.com>
2024-11-22 20:23:02 +0500

    Update Dishescard.tsx

--- src/app/components/assets/Dishescard.tsx
+++ src/app/components/assets/Dishescard.tsx
@@ -1,23 +1,30 @@
-import Image from 'next/image'
-import React from 'react'
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
 
-const Dishescard = ({project}) => {
+type ProjectType = {
+  title: string;
+  image: string | StaticImageData;
+  description: string;
+};
+
+const Dishescard: React.FC<{ project: ProjectType }> = ({ project }) => {
   return (
     <div>
-        <Image
+      <Image
         src={project.image}
         alt={project.title}
-        className='rounded-3xl p-2'
-        />
-        <div className='p-4'>
-            <h3 className='mb-2 text-2xl font-bold tracking-tighter'>
-                {project.title}
-            </h3>
-            <p className='text-sm'>{project.description}</p>
-
-        </div>
+        width={300}
+        height={300}
+        className="rounded-3xl p-2"
+      />
+      <div className="p-4">
+        <h3 className="mb-2 text-2xl font-bold tracking-tighter">
+          {project.title}
+        </h3>
+        <p className="text-sm">{project.description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dishescard
+export default Dishescard;
