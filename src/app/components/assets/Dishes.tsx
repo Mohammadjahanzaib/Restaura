f90ead5629c0459fe4c96481a2b2d4c1182289d9
Mohammadjahanzaib <157308796+Mohammadjahanzaib@users.noreply.github.com>
2024-11-22 20:23:56 +0500

    Update Dishes.tsx

--- src/app/components/assets/Dishes.tsx
+++ src/app/components/assets/Dishes.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Dishescard from './Dishescard'
-import { DISHES } from '../../constants'
+import React from 'react';
+import Dishescard from './Dishescard';
+import { DISHES } from '../../constants';
 
 const Dishes = () => {
   return (
-    <section className='container mx-auto py-16 text-white' id="dishes">
-        <h2 className='mb-8 text-center text-3xl tracking-tighter lg:text-4xl'>Our Dishes</h2>
-    <div className='grid grid-cols-1 gap-8 md:grid-cols-5'>
+    <section className="container mx-auto py-16 text-white" id="dishes">
+      <h2 className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl">
+        Our Dishes
+      </h2>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
         {DISHES.map((project, index) => (
-            <Dishescard key={index} project={project}/>
+          <Dishescard key={index} project={project} />
         ))}
-
-    </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Dishes
+export default Dishes;
